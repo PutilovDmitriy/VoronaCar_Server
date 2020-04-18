80a684b3a8eece554d4be09d9faaf3d3630e730b
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-04-18 19:15:20 +0500

    update parser

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/auth", require("./app/routes/user.auth"));
 app.use("/user", require("./app/routes/user.info"));
 app.use("/car", require("./app/routes/cars"));
