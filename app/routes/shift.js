41aa875f53a6cd09056b9606531c88ea0c6c819d
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-04-19 09:35:27 +0500

    add update shift

--- app/routes/shift.js
+++ app/routes/shift.js
@@ -48,14 +48,59 @@ router.post(
   }
 );
 
+//../shift/update
+router.put(
+  "/update",
+  [
+    check("shiftId", "Отсутствует Id").exists(),
+    check("carNumber", "Список машин отсутствует").exists(),
+    check("value", "Отсутсвует колличество литров").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: "Некорректные данные при обновлении смены",
+      });
+    }
+
+    try {
+      const { shiftId, carNumber, value } = req.body;
+
+      const shiftS = await Shift.findOne({ _id: shiftId });
+
+      if (!shiftS) {
+        return res.status(400).json({ message: "Такая смена не найдена" });
+      }
+
+      const valueOil = Number(shiftS.valueOil) + Number(value);
+      const carsList = [...shiftS.carList, carNumber];
+
+      const shift = new Shift({
+        _id: shiftS._id,
+        userId: shiftS.userId,
+        shiftStart: shiftS.shiftStart,
+        shiftTime: 0,
+        valueOil: valueOil,
+        carsList: carsList,
+        isFinished: true,
+      });
+
+      await shift.replaceOne(shift);
+
+      return res.status(202).json({ message: "Данные о смене обновлены" });
+    } catch (e) {
+      res.status(500).json({ message: "Что то пошло не так" });
+    }
+  }
+);
+
 //../shift/finish
 router.put(
   "/finish",
-  [
-    check("shiftId", "Отсутствует Id").exists(),
-    check("carsList", "Список машин отсутствует").isArray(),
-    check("value", "Отсутсвует колличество литров").exists(),
-  ],
+  [check("shiftId", "Отсутствует Id").exists()],
   async (req, res) => {
     const errors = validationResult(req);
 
@@ -67,7 +112,7 @@ router.put(
     }
 
     try {
-      const { shiftId, carsList, value } = req.body;
+      const { shiftId } = req.body;
 
       const shiftS = await Shift.findOne({ _id: shiftId });
 
@@ -85,8 +130,8 @@ router.put(
         userId: shiftS.userId,
         shiftStart: dateS,
         shiftTime: m,
-        valueOil: Number(value),
-        carsList: carsList,
+        valueOil: shiftS.valueOil,
+        carsList: shiftS.carsList,
         isFinished: true,
       });
 
