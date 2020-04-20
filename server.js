0ecd0153bf8e2014c8ddc7dda86af815d9c4bcd1
PutilovDmitriy <dmitrii.98.putilov@gmail.com>
2020-04-20 22:55:13 +0500

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const config = require("config");
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/auth", require("./app/routes/user.auth"));
