80167b6fc174d0dca43386d67877620204fcaec0
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-05-06 13:22:13 +0500

    add  upload  photos

--- app/routes/cars.js
+++ app/routes/cars.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
+const fs = require("fs");
+const path = require("path");
+const rimraf = require("rimraf");
+const multer = require("multer");
+const uuidv4 = require("uuid").v4;
 const Car = require("../models/Car");
 const Vorona = require("../models/Vorona");
 const router = Router();
@@ -59,7 +64,9 @@ router.post(
 
       const send = await car.save();
 
-      return res.status(201).json(send);
+      fs.mkdir(`./public/${send.number}`, () => {
+        return res.status(201).json(send);
+      });
     } catch (e) {
       res.status(500).json({ message: "Что-то пошло не так" });
     }
@@ -181,11 +188,93 @@ router.delete("/:number", async (req, res) => {
 
     await Car.remove({ _id: candidate._id });
 
+    rimraf(`public/${userId}/${_id}`, () => console.log("Папка удалена"));
     return res.status(200).json({ message: "Авто удалено удален" });
   } catch (e) {
     res.status(500).json({ message: "Что то пошло не так" });
   }
 });
+
+//upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = `./public/${req.params.number}`;
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const fileName = String(new Date()).toLowerCase().split(" ").join("-");
+    cb(null, uuidv4() + "-" + fileName);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
+
+clearDir = async (req, res, next) => {
+  try {
+    const dir = `./public/${req.params.number}`;
+
+    const car = Car.findOne({ userId: req.headers.userid });
+    fs.readdir(dir, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        car.photos.map((url) => {
+          if (!url.includes(file)) {
+            fs.unlink(path.join(dir, file), (err) => {
+              if (err) throw err;
+            });
+          }
+        });
+      }
+    });
+    next();
+  } catch {
+    return res.status(500).send("Что-то пошло не так");
+  }
+};
+
+//../car/upload-images
+router.post(
+  "/upload-file/:number",
+  clearDir,
+  upload.array("imgCollection", 6),
+  async (req, res) => {
+    try {
+      const number = req.params.number;
+      const reqFiles = [];
+      const url = "https://pacific-cliffs-72324.herokuapp.com/public";
+      req.files.map((file) => {
+        reqFiles.push(url + `/${number}/` + file.filename);
+      });
+
+      let car = Car.findOne({ number });
+
+      car.photos = [...reqFiles, ...car.photos];
+
+      await Car.update({ _id: car._id }, car);
+
+      const send = Car.findOne({ _id: car._id });
+
+      return res.status(200).send(send);
+    } catch {
+      return res.status(500).send("Что-то пошло не так");
+    }
+  }
+);
 
 //..car/vorona
 router.get("/vorona", async (req, res) => {
