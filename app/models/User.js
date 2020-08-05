31523d7a9167eec1dd8e3f3e0d7ac9f293324205
PutilovDmitriy <dmitrii.98.putilov@gmail.com>
2020-08-05 22:00:50 +0500

    update user schema

--- app/models/User.js
+++ app/models/User.js
@@ -14,6 +14,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  admin: {
+    type: Boolean,
+    required: false,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
