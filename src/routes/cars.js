04a5f3e6a6db7bf568f619434ce0948d7a3916fa
PutilovDmitriy <44982167+PutilovDmitriy@users.noreply.github.com>
2020-10-29 17:00:56 +0500

    add post events

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -296,6 +296,44 @@ router.get("/events/:number", async (req, res) => {
   }
 });
 
+router.post("/events/:number", async (req, res) => {
+  try {
+    const { number } = req.params;
+
+    const car = await Car.findOne({ number });
+    if (!car) {
+      return res.status(400).json({ message: "Такая машина не найдена" });
+    }
+    if (!car.events || car.events.length === 0) {
+      return res.status(400).json({ message: "У авто нет доступных событий" });
+    }
+
+    const workbook = new ExcelJs.Workbook();
+    const worksheet = workbook.addWorksheet(`События по ${car.number}`);
+
+    worksheet.columns = [
+      { header: "№", key: "id", width: 10 },
+      { header: "Дата", key: "date", width: 32 },
+      { header: "Пробег", key: "mileage", width: 32 },
+      { header: "Событие", key: "event", width: 150 },
+    ];
+    car.events.forEach((event, index) => {
+      worksheet.addRow({
+        id: index + 1,
+        date: new Intl.DateTimeFormat("ru").format(new Date(event.date)),
+        mileage: event.mileage,
+        event: event.text,
+      });
+    });
+
+    const tempFilePath = tempfile(".xlsx");
+    await workbook.xlsx.writeFile(tempFilePath);
+    return res.status(200).sendFile(tempFilePath);
+  } catch {
+    res.status(500).json({ message: "Что-то пошло не так" });
+  }
+});
+
 //..car/vorona
 router.get("/vorona", async (req, res) => {
   try {
